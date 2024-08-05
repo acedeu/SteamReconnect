4bac002737c409a480bdeab584223881c25fed10
aced <104261012+acedeu@users.noreply.github.com>
2024-08-05 02:39:02 +0100

    Imported SteamReconnect.cs

---
+++ SteamReconnect.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using Oxide.Core.Plugins;
+using Steamworks;
+
+namespace Oxide.Plugins
+{
+    [Info("SteamReconnect", "aced", "1.0.0")]
+    [Description("If the server loses connection to Steam, it will attempt to reconnect every 60 seconds.")]
+    public class SteamReconnect : RustPlugin
+    {
+        [UsedImplicitly]
+        [HookMethod(nameof(Init))]
+        private void Init()
+        {
+            timer.Every(60f, () =>
+            {
+                if (SteamServer.LoggedOn == false)
+                {
+                    SteamServer.LogOnAnonymous();
+                }
+            });
+        }
+    }
+}
